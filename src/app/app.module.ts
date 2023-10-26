7d766a789ee9451cfb9a3f9cde90ef078120d859
delphine <delphine.boitelleparsy@gmail.com>
2023-10-26 14:30:00 +0200

    ajouterProduit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,36 @@ import {FormsModule} from "@angular/forms";
 import {LoginComponent} from "./login/login.component";
 import {ManagerPageComponent} from "./manager-page/manager-page.component";
 import {ProduitMockService} from "./produit/produit.mock.service";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { DialogAjouterProduitComponent } from './dialog-ajouter-produit/dialog-ajouter-produit.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgIf } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ManagerPageComponent
+    ManagerPageComponent,
+    DialogAjouterProduitComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    NgIf,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule
+
   ],
   providers: [ProduitMockService],
   bootstrap: [AppComponent]
--- src/app/manager-page/manager-page.component.ts
+++ src/app/manager-page/manager-page.component.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 import { ProduitMockService } from '../produit/produit.mock.service';
 import { produit } from '../shared/produit';
 import { Router } from '@angular/router';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAjouterProduitComponent } from '../dialog-ajouter-produit/dialog-ajouter-produit.component';
 
 @Component({
   selector: 'app-manager-page',
@@ -11,13 +13,29 @@ import { Router } from '@angular/router';
 export class ManagerPageComponent implements OnInit {
   compteur: number = 0;
   produits: produit[];
+  
 
-  constructor(private el: ElementRef, private produitService: ProduitMockService, private router: Router) { 
+  constructor(
+    private el: ElementRef, 
+    private produitService: ProduitMockService, 
+    private router: Router,
+    public dialog : MatDialog
+    ) { 
     this.produits = [];
   }
 
   ngOnInit(){
     this.produits = this.produitService.getProduits();
+}
+
+openDialog(): void {
+  const dialogRef = this.dialog.open(DialogAjouterProduitComponent, {
+    width: '500px', height : '350px'
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    console.log('The dialog was closed');
+  });
 }
 
   enleverProduit(){
@@ -28,9 +46,6 @@ export class ManagerPageComponent implements OnInit {
 
   ajouterProduit(){
     this.compteur++;
-  }
-
-  newProduct(){
   }
 
   supprimerProduit() {
@@ -44,3 +59,4 @@ export class ManagerPageComponent implements OnInit {
     this.router.navigate(['/']);
   }
 }
+

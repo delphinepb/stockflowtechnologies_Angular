c215aae553ec44bcf1017fd3eb9dcd4aabb93ff1
metrike <kevinmetri.pro@gmail>
2023-10-26 13:46:00 +0200

    + et - produit

---
+++ src/app/updateProduit.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class updateProduit {
+    private baseUrl = 'https://localhost:7170';
+
+    constructor(private http: HttpClient) { }
+
+    authenticate(id: any,quantite:any) {
+        const credentials = { id, quantite };
+        return this.http.post(`${this.baseUrl}/UpdateQuantite`, credentials);
+    }
+}
